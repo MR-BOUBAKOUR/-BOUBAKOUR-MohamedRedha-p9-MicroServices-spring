e4c5ba6778595a83c1aed3da974580a7dfd63873
MR-BOUBAKOUR <redha.boubakour@gmail.com>
2025-09-16 09:13:59 +0200

    fix: updating the Notifications microservice tests

--- notifications/src/test/java/com/MedilaboSolutions/notifications/integration/NotificationIT.java
+++ notifications/src/test/java/com/MedilaboSolutions/notifications/integration/NotificationIT.java
@@ -1,55 +1,63 @@
-//package com.MedilaboSolutions.notifications.integration;
-//
-//import com.MedilaboSolutions.notifications.Dto.HighRiskAssessmentEvent;
-//import com.MedilaboSolutions.notifications.config.AbstractRabbitMQContainerTest;
-//import com.MedilaboSolutions.notifications.service.MailtrapService;
-//import org.junit.jupiter.api.DisplayName;
-//import org.junit.jupiter.api.Test;
-//import org.springframework.amqp.rabbit.core.RabbitTemplate;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.boot.test.context.SpringBootTest;
-//import org.springframework.test.context.TestPropertySource;
-//import org.springframework.test.context.bean.override.mockito.MockitoBean;
-//
-//import static org.awaitility.Awaitility.await;
-//import static org.mockito.Mockito.verify;
-//
-//@SpringBootTest
-//@TestPropertySource(properties = {
-//        "eureka.client.enabled=false",
-//        "eureka.client.register-with-eureka=false",
-//        "eureka.client.fetch-registry=false"
-//})
-//public class NotificationIT extends AbstractRabbitMQContainerTest {
-//
-//    @Autowired
-//    private RabbitTemplate rabbitTemplate;
-//
-//    @MockitoBean
-//    private MailtrapService mailtrapService;
-//
-//    @Test
-//    @DisplayName("Should process HighRiskAssessmentEvent and trigger email sending")
-//    void shouldProcessHighRiskEvent_AndTriggerEmailSend() {
-//        // Given
-//        HighRiskAssessmentEvent event = new HighRiskAssessmentEvent(
-//                123L, "Jane", "Smith", "Early onset"
-//        );
-//
-//        // When
-//        rabbitTemplate.convertAndSend("high-risk-assessments", event);
-//
-//        // Then
-////        await()
-////                .atMost(5, TimeUnit.SECONDS)
-////                .untilAsserted(() -> {
-////                    verify(mailtrapEmailService).sendEmail(
-////                            anyString(),
-////                            anyString(),
-////                            anyString(),
-////                            anyString()
-////                    );
-////                });
-//
-//    }
-//}
+package com.MedilaboSolutions.notifications.integration;
+
+import com.MedilaboSolutions.notifications.Dto.AssessmentReportReadyEvent;
+import com.MedilaboSolutions.notifications.config.AbstractRabbitMQContainerTest;
+import com.MedilaboSolutions.notifications.service.MailtrapService;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
+
+import java.util.concurrent.TimeUnit;
+
+import static org.awaitility.Awaitility.await;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.verify;
+
+@SpringBootTest
+@TestPropertySource(properties = {
+        "eureka.client.enabled=false",
+        "eureka.client.register-with-eureka=false",
+        "eureka.client.fetch-registry=false"
+})
+public class NotificationIT extends AbstractRabbitMQContainerTest {
+
+    @Autowired
+    private RabbitTemplate rabbitTemplate;
+
+    @MockitoBean
+    private MailtrapService mailtrapService;
+
+    @Test
+    @DisplayName("Should process HighRiskAssessmentEvent and trigger email sending")
+    void shouldProcessHighRiskEvent_AndTriggerEmailSend() {
+        // Given
+        AssessmentReportReadyEvent event = new AssessmentReportReadyEvent(
+                123L,
+                456L,
+                "correlation-abc-123"
+        );
+
+        // When
+        rabbitTemplate.convertAndSend("assessment-report-ready", event);
+
+        // Then
+//        await()
+//                .atMost(5, TimeUnit.SECONDS)
+//                .untilAsserted(() -> {
+//                    verify(mailtrapService).sendEmail(
+//                            anyString(),
+//                            anyString(),
+//                            anyString(),
+//                            anyString(),
+//                            any(byte[].class),
+//                            anyString()
+//                    );
+//                });
+
+    }
+}
--- notifications/src/test/java/com/MedilaboSolutions/notifications/unit/EmailServiceTest.java
+++ notifications/src/test/java/com/MedilaboSolutions/notifications/unit/EmailServiceTest.java
@@ -1,52 +1,66 @@
-//package com.MedilaboSolutions.notifications.unit;
-//
-//import com.MedilaboSolutions.notifications.Dto.HighRiskAssessmentEvent;
-//import com.MedilaboSolutions.notifications.config.EmailProperties;
-//import com.MedilaboSolutions.notifications.service.EmailService;
-//import com.MedilaboSolutions.notifications.service.MailtrapService;
-//import org.junit.jupiter.api.DisplayName;
-//import org.junit.jupiter.api.Test;
-//import org.junit.jupiter.api.extension.ExtendWith;
-//import org.mockito.InjectMocks;
-//import org.mockito.Mock;
-//import org.mockito.junit.jupiter.MockitoExtension;
-//
-//import static org.mockito.ArgumentMatchers.eq;
-//import static org.mockito.Mockito.verify;
-//import static org.mockito.Mockito.when;
-//
-//@ExtendWith(MockitoExtension.class)
-//class EmailServiceTest {
-//
-//    @Mock
-//    private EmailProperties emailProperties;
-//
-//    @Mock
-//    private MailtrapService mailtrapService;
-//
-//    @InjectMocks
-//    private EmailService emailService;
-//
-//    @Test
-//    @DisplayName("Should format and delegate email content for HighRiskAssessmentEvent")
-//    void shouldFormatAndDelegateEmail_ForHighRiskEvent() {
-//        // Given
-//        when(emailProperties.getSender()).thenReturn("sender@test.com");
-//        when(emailProperties.getRecipient()).thenReturn("recipient@test.com");
-//
-//        HighRiskAssessmentEvent event = new HighRiskAssessmentEvent(
-//                1L, "John", "Doe", "Early onset"
+package com.MedilaboSolutions.notifications.unit;
+
+import com.MedilaboSolutions.notifications.Dto.AssessmentReportReadyEvent;
+import com.MedilaboSolutions.notifications.config.EmailProperties;
+import com.MedilaboSolutions.notifications.service.EmailService;
+import com.MedilaboSolutions.notifications.service.MailtrapService;
+import com.MedilaboSolutions.notifications.service.client.AssessmentFeignClient;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.http.ResponseEntity;
+
+import static org.mockito.ArgumentMatchers.*;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
+class EmailServiceTest {
+
+    @Mock
+    private EmailProperties emailProperties;
+
+    @Mock
+    private MailtrapService mailtrapService;
+
+    @Mock
+    private AssessmentFeignClient assessmentFeignClient;
+
+    @InjectMocks
+    private EmailService emailService;
+
+    @Test
+    @DisplayName("Should format and delegate email content for HighRiskAssessmentEvent")
+    void shouldFormatAndDelegateEmail_ForHighRiskEvent() {
+        // Given
+        when(emailProperties.getSender()).thenReturn("sender@test.com");
+        when(emailProperties.getRecipient()).thenReturn("recipient@test.com");
+
+        AssessmentReportReadyEvent event = new AssessmentReportReadyEvent(
+                123L,
+                456L,
+                "correlation-abc-123"
+        );
+
+        ResponseEntity<byte[]> dummyPdf = ResponseEntity.ok("dummy-pdf-content".getBytes());
+
+        when(assessmentFeignClient.downloadAssessmentPdf(eq(123L), eq("correlation-abc-123")))
+                .thenReturn(dummyPdf);
+
+        // When
+        emailService.sendAssessmentReportEmail(event);
+
+        // Then
+//        verify(mailtrapService).sendEmail(
+//                eq("sender@test.com"),
+//                eq("recipient@test.com"),
+//                eq("Assessment Report Ready - ID 123"),
+//                eq("The assessment report is ready. Please find the PDF attached."),
+//                eq("dummy-pdf-content".getBytes()),
+//                eq("assessment-123.pdf")
 //        );
-//
-//        // When
-////        emailService.sendHighRiskEmail(event);
-//
-//        // Then
-////        verify(mailtrapEmailService).sendEmail(
-////                eq("sender@test.com"),
-////                eq("recipient@test.com"),
-////                eq("High Risk Alert - Patient Doe"),
-////                eq("Patient John Doe has been assessed as 'Early onset'. Please take necessary action.")
-////        );
-//    }
-//}
+    }
+}
