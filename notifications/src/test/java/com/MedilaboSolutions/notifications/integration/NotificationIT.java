186db08d0a2eae735cdeb2daee25d38d29655a4f
MR-BOUBAKOUR <redha.boubakour@gmail.com>
2025-09-17 14:03:54 +0200

    fix: mocking assessmentFeignClient ; updating the POM file of notifications to exclude the right third party class (MailtrapService.class) from the test coverage

--- notifications/src/test/java/com/MedilaboSolutions/notifications/integration/NotificationIT.java
+++ notifications/src/test/java/com/MedilaboSolutions/notifications/integration/NotificationIT.java
@@ -3,11 +3,13 @@ package com.MedilaboSolutions.notifications.integration;
 import com.MedilaboSolutions.notifications.Dto.AssessmentReportReadyEvent;
 import com.MedilaboSolutions.notifications.config.AbstractRabbitMQContainerTest;
 import com.MedilaboSolutions.notifications.service.MailtrapService;
+import com.MedilaboSolutions.notifications.service.client.AssessmentFeignClient;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
 
@@ -17,6 +19,7 @@ import static org.awaitility.Awaitility.await;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
 @SpringBootTest
 @TestPropertySource(properties = {
@@ -32,6 +35,9 @@ public class NotificationIT extends AbstractRabbitMQContainerTest {
     @MockitoBean
     private MailtrapService mailtrapService;
 
+    @MockitoBean
+    private AssessmentFeignClient assessmentFeignClient;
+
     @Test
     @DisplayName("Should process HighRiskAssessmentEvent and trigger email sending")
     void shouldProcessHighRiskEvent_AndTriggerEmailSend() {
@@ -42,22 +48,25 @@ public class NotificationIT extends AbstractRabbitMQContainerTest {
                 "correlation-abc-123"
         );
 
+        when(assessmentFeignClient.downloadAssessmentPdf(123L, "correlation-abc-123"))
+                .thenReturn(ResponseEntity.ok("dummy-pdf-content".getBytes()));
+
         // When
         rabbitTemplate.convertAndSend("assessment-report-ready", event);
 
         // Then
-//        await()
-//                .atMost(5, TimeUnit.SECONDS)
-//                .untilAsserted(() -> {
-//                    verify(mailtrapService).sendEmail(
-//                            anyString(),
-//                            anyString(),
-//                            anyString(),
-//                            anyString(),
-//                            any(byte[].class),
-//                            anyString()
-//                    );
-//                });
+        await()
+                .atMost(5, TimeUnit.SECONDS)
+                .untilAsserted(() -> {
+                    verify(mailtrapService).sendEmail(
+                            anyString(),
+                            anyString(),
+                            anyString(),
+                            anyString(),
+                            any(byte[].class),
+                            anyString()
+                    );
+                });
 
     }
 }
